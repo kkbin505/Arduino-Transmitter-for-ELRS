2a91fbdfd442288e81ab6560685b308e8f6344e0
kkbin505 <43392862+kkbin505@users.noreply.github.com>
2022-01-29 22:10:29 +0800

    Update config.h

--- Simple_TX/config.h
+++ Simple_TX/config.h
@@ -38,6 +38,8 @@ const int VOLTAGE_READ_PIN = A0;
 //pins that used for the switch
 const int DIGITAL_PIN_SWITCH_ARM = 4;  // Arm switch
 const int DIGITAL_PIN_SWITCH_AUX2 = 3;  // 
+const int DIGITAL_PIN_SWITCH_AUX3 = 5;  // 
+const int DIGITAL_PIN_SWITCH_AUX4 = 6;  // 
 
 //pins that used for output
 const int DIGITAL_PIN_LED = 5;  // 
@@ -47,4 +49,41 @@ const int DIGITAL_PIN_BUZZER = 7;  //
 #define VOLTAGE_READS 10 //get average of VOLTAGE_READS readings
 
  // Define battery warning voltage
- const float WARNING_VOLTAGE=7.2; //2S Lipo
+ const float WARNING_VOLTAGE=7.4; //2S Lipo
+
+ // Define Commond for start Up Setting
+ #define RC_MIN_COMMAND  480
+ #define RC_MAX_COMMAND  1500
+
+// from https://github.com/DeviationTX/deviation/pull/1009/ ELRS menu implement in deviation TX
+/*static uint8_t  currentPktRate =1; //  "250Hz", "150Hz", "50Hz"
+  //                                      1        3       5      
+static uint8_t  currentPower =1 ;//  "10mW", "25mW", "50mW", "100mW", "250mW"
+  //                                   0     1         2        3        4   
+static uint8_t currentTlmRatio =0 ;
+static uint8_t currentBind = 0;
+static uint8_t currentWiFi = 0;
+static uint8_t getParamsCounter = 0;
+static uint8_t currentFrequency = 6; //2.4G
+*/
+//2 Default Settings
+#define SETTING_1_PktRate 1  //250Hz
+#define SETTING_1_Power 2    //50mW
+
+#define SETTING_2_PktRate 3  //150Hz
+#define SETTING_2_Power 3    //100wW
+
+enum chan_order{
+    AILERON,
+    ELEVATOR,
+    THROTTLE, 
+    RUDDER,
+    AUX1,  // (CH5)  led light, or 3 pos. rate on CX-10, H7, or inverted flight on H101
+    AUX2,  // (CH6)  flip control
+    AUX3,  // (CH7)  still camera (snapshot)
+    AUX4,  // (CH8)  video camera
+    AUX5,  // (CH9)  headless
+    AUX6,  // (CH10) calibrate Y (V2x2), pitch trim (H7), RTH (Bayang, H20), 360deg flip mode (H8-3D, H22)
+    AUX7,  // (CH11) calibrate X (V2x2), roll trim (H7), emergency stop (Bayang, Silverware)
+    AUX8,  // (CH12) Reset / Rebind
+};
