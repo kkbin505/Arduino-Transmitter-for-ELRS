80f64df84d3c200250c019df7154f35ad693e9a2
kkbin505 <43392862+kkbin505@users.noreply.github.com>
2022-01-29 22:09:54 +0800

    Add gesutre to change settings.

--- Simple_TX/Simple_TX.ino
+++ Simple_TX/Simple_TX.ino
@@ -40,11 +40,18 @@ int Elevator_value = 0;
 int Throttle_value=0;
 int Rudder_value = 0; 
 
+int loopCount=0;
+
 int Arm = 0;        // switch values read from the digital pin
-int FlightMode = 0; 
+int AUX2_value = 0; 
+int AUX3_value = 0; 
+int AUX4_value = 0; 
 
 float batteryVoltage;
 
+int currentPktRate = 0;
+int currentPower = 0;
+int currentSetting = 0;
 
 uint8_t crsfPacket[CRSF_PACKET_SIZE];
 int rcChannels[CRSF_MAX_CHANNEL];
@@ -55,15 +62,17 @@ void setup() {
     for (uint8_t i = 0; i < CRSF_MAX_CHANNEL; i++) {
         rcChannels[i] = RC_CHANNEL_MIN;
     }
-   //analogReference(EXTERNAL);
+   analogReference(EXTERNAL);
    pinMode(DIGITAL_PIN_SWITCH_ARM, INPUT_PULLUP);
    pinMode(DIGITAL_PIN_SWITCH_AUX2, INPUT_PULLUP);
+   //pinMode(DIGITAL_PIN_SWITCH_AUX3, INPUT_PULLUP);
+   pinMode(DIGITAL_PIN_SWITCH_AUX4, INPUT_PULLUP);
    pinMode(DIGITAL_PIN_LED, OUTPUT);//LED
    pinMode(DIGITAL_PIN_BUZZER, OUTPUT);//LED
    //digitalWrite(DIGITAL_PIN_BUZZER, LOW);
    batteryVoltage=7.0; 
    
-   delay(3000); //Give enough time for uploda firmware
+   delay(1000); //Give enough time for uploda firmware
    Serial.begin(SERIAL_BAUDRATE);
    // Serial.write(crsfPacket1, CRSF_PACKET_SIZE);
 
@@ -75,8 +84,9 @@ void setup() {
 
 void loop() {
     uint32_t currentMicros = micros();
-    
-    batteryVoltage=readVoltage();
+
+    //Read Voltage
+    batteryVoltage=analogRead(VOLTAGE_READ_PIN)/103.0;
 
     if (batteryVoltage<WARNING_VOLTAGE){
        fastBlinkLED(DIGITAL_PIN_LED);
@@ -90,30 +100,59 @@ void loop() {
     Throttle_value=analogRead(analogInPinThrottle); 
     Rudder_value = constrain(analogRead(analogInPinRudder),209,853);  //My gimbal do not center, this function constrain end.
     Arm = digitalRead(DIGITAL_PIN_SWITCH_ARM);
-    FlightMode = digitalRead(DIGITAL_PIN_SWITCH_AUX2);
-    rcChannels[0] = map(Aileron_value,893,223,RC_CHANNEL_MIN,RC_CHANNEL_MAX); //reverse
-    rcChannels[1] = map(Elevator_value,130,778,RC_CHANNEL_MIN,RC_CHANNEL_MAX); 
-    rcChannels[2] = map(Throttle_value,855,183,RC_CHANNEL_MIN,RC_CHANNEL_MAX);//reverse
-    rcChannels[3] = map(Rudder_value ,209,853,RC_CHANNEL_MIN,RC_CHANNEL_MAX);
-	
+    AUX2_value = digitalRead(DIGITAL_PIN_SWITCH_AUX2);
+    //AUX3 = digitalRead(DIGITAL_PIN_SWITCH_AUX3);
+    AUX4_value = digitalRead(DIGITAL_PIN_SWITCH_AUX4);
+    rcChannels[AILERON] = map(Aileron_value,893,223,RC_CHANNEL_MIN,RC_CHANNEL_MAX); //reverse
+    rcChannels[ELEVATOR] = map(Elevator_value,130,778,RC_CHANNEL_MIN,RC_CHANNEL_MAX); 
+    rcChannels[THROTTLE] = map(Throttle_value,855,183,RC_CHANNEL_MIN,RC_CHANNEL_MAX);//reverse
+    rcChannels[RUDDER] = map(Rudder_value ,209,853,RC_CHANNEL_MIN,RC_CHANNEL_MAX);
+
+    
 	//Aux 1 Arm Channel
     if(Arm==0){
-      rcChannels[4] =RC_CHANNEL_MIN;
+      rcChannels[AUX1] =RC_CHANNEL_MIN;
     }else if(Arm==1){
-      rcChannels[4] =RC_CHANNEL_MAX;
+      rcChannels[AUX1] =RC_CHANNEL_MAX;
     }
 
-	//Aux 2 Mode Channel
-    if(FlightMode==1){
-      rcChannels[5] =RC_CHANNEL_MIN;
-    }else if(FlightMode==0){
-      rcChannels[5] =RC_CHANNEL_MAX;
+	//Aux 2 Channel
+    if(AUX2_value==1){
+      rcChannels[AUX2] =RC_CHANNEL_MIN;
+    }else if(AUX2_value==0){
+      rcChannels[AUX2] =RC_CHANNEL_MAX;
+    }
+
+    //Aux 4
+    if(AUX4_value==1){
+      rcChannels[AUX4] =RC_CHANNEL_MIN;
+    }else if(AUX4_value==0){
+      rcChannels[AUX4] =RC_CHANNEL_MAX;
     }
 
 	//Additional switch add here.
 //  transmit_enable=!digitalRead(transmit_pin);
-  
+
+   selectProtocol();
+  // currentSetting=1;
     if (currentMicros > crsfTime) {
+
+      if (loopCount<=200){
+        //Build commond packet
+        if (currentSetting >0){
+          buildElrsPacket(crsfCmdPacket,ELRS_PKT_RATE_COMMAND,currentPktRate);
+          Serial.write(crsfCmdPacket, CRSF_CMD_PACKET_SIZE);
+        }
+        loopCount++;
+      }
+      else if (loopCount > 200 && loopCount < 400){
+        if (currentSetting <0){
+          buildElrsPacket(crsfCmdPacket,ELRS_POWER_COMMAND,currentPower);
+          Serial.write(crsfCmdPacket, CRSF_CMD_PACKET_SIZE);
+        }
+        loopCount++;
+      }
+      else{
         crsfPreparePacket(crsfPacket, rcChannels);
       //For gimal calibation only
 	  #ifdef DEBUG
@@ -136,15 +175,35 @@ void loop() {
         Serial.print(";Arm_");  
         Serial.print(Arm);
         Serial.print(";Mode_");  
-        Serial.print(FlightMode);
+        Serial.print(AUX2);
         Serial.print("_BatteryVoltage:");  
         Serial.print(batteryVoltage);
+        Serial.print("_CurrentSetting:");  
+        Serial.print(currentSetting);
         Serial.println();  
         delay(500);
        #else
          Serial.write(crsfPacket, CRSF_PACKET_SIZE);
        #endif
-       
+      }
         crsfTime = currentMicros + CRSF_TIME_BETWEEN_FRAMES_US;
     }
+}
+
+void selectProtocol(){
+
+    currentSetting =0;
+    // startup stick commands (protocol selection / renew transmitter ID)
+    
+    if(rcChannels[AILERON] < RC_MIN_COMMAND && rcChannels[ELEVATOR] < RC_MIN_COMMAND){ // Elevator down + aileron left
+      currentPktRate = SETTING_1_PktRate;
+      currentPower = SETTING_1_Power;
+      currentSetting =1;
+    }
+    else if (rcChannels[AILERON] > RC_MAX_COMMAND && rcChannels[ELEVATOR] > RC_MAX_COMMAND){ // Elevator up + aileron right
+      currentPktRate = SETTING_2_PktRate;
+      currentPower = SETTING_2_Power;
+      currentSetting =2;
+    }
+    //reture Setting;
 }
