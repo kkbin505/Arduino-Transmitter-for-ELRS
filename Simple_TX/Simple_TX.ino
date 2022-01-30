def5fafab0cc778463342464ea86dffaea8a1cdd
kkbin505 <43392862+kkbin505@users.noreply.github.com>
2022-01-30 15:06:12 +0800

    Update Simple_TX.ino

--- Simple_TX/Simple_TX.ino
+++ Simple_TX/Simple_TX.ino
@@ -4,7 +4,7 @@
 // Arduino Nano
 // ELRS 2.4G TX moduel
 // Custom PCB from JLCPCB
-// const float codeVersion = 0.91; // Software revision
+// const float codeVersion = 0.9.1; // Software revision
 // https://github.com/kkbin505/Arduino-Transmitter-for-ELRS
 
  * This file is part of Simple TX
